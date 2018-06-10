5471e29e7f8efc5fce2ca09052fb710c92292307
Sy Ky <caosyky.1994@gmail.com>
2018-06-10 22:32:10 +0700

    them con tro

--- 1.js
+++ 1.js
@@ -1,4 +1,9 @@
 ﻿ $(function(){
- 
+ 	vitrikinhnghiem = $('#kinhnghiem').offset().top;
+ 	console.log(vitrikinhnghiem);
+ 	$(".conchuot,.kinhnghiem").click(function(){
+ 		$('body,html').animate({scrollTop:vitrikinhnghiem},700)
+ 		return false;
+ 	})
 })  
  
